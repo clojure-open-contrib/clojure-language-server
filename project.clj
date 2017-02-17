8f953a76c14ebc67df0bb6ff6a46a73db60e3ee0
BlankRain <731014656@qq.com>
2017-02-17 11:09:42 +0800

    嗯

--- project.clj
+++ project.clj
@@ -6,6 +6,16 @@
                        :url "http://maven.aliyun.com/nexus/content/groups/public"}}
   :repositories {"aliyun" {:url "http://maven.aliyun.com/nexus/content/groups/public"}
                  "maven"   {:url "http://repo1.maven.org/maven2/"}}
-  :main clojure-language-server.core
-  :aot [clojure-language-server.core]
-  :dependencies [[org.clojure/clojurescript "1.9.473"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.473"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :cljsbuild {}
+    :builds [{}]
+        ; The path to the top-level ClojureScript source directory:
+        :source-paths ["src"]
+        ; The standard ClojureScript compiler options:
+        ; (See the ClojureScript compiler documentation for details.)
+        :compiler {}
+          :output-to "main.js"  ; default: target/cljsbuild-main.js
+          :optimizations :whitespace
+          :pretty-print true)
